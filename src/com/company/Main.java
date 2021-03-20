d5a6404fe9e41dbe03ed418f7af3ccc51c7c48a9
arnayots <ar.stoyan@gmail.com>
2021-03-21 01:44:19 +0200

    lexer v1.4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         try{
-
+            lexer a = new lexer("D:\\DATA\\Program Lab&HW\\Lab 3\\6\\Code\\lexer.cpp");
         }
         catch (IOException e){
             e.printStackTrace();
--- src/com/company/lexer.java
+++ src/com/company/lexer.java
@@ -1,7 +1,8 @@
 package com.company;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Locale;
 
 public class lexer {
 
@@ -11,31 +12,199 @@ public class lexer {
         fr = new FileReader(file);
         reader = new BufferedReader(fr);
 
-        //directives
-/*
-        File file1 = new File(directibes_file);
+        HashSet<Integer> white = new HashSet<>();
+        HashSet<Integer> az = new HashSet<>();
+        HashSet<Integer> az09 = new HashSet<>();
+        HashSet<Integer> az09_ = new HashSet<>();
+        HashSet<String> operator = new HashSet<>();
+        HashSet<String> reserved = new HashSet<>();
+
+        white.add(011);
+        white.add(012);
+        white.add(013);
+        white.add(014);
+        white.add(015);
+        white.add(040);
+
+        for(int i = 'a'; i <= 'z'; i++){
+            az.add(i);
+            az09.add(i);
+            az09_.add(i);
+        }
+        for(int i = '0'; i <= '9'; i++){
+            az09.add(i);
+            az09_.add(i);
+        }
+        az09_.add((int)'_');
+
+        File file1 = new File("operators.txt");
         FileReader fr1 = new FileReader(file1);
         BufferedReader reader1 = new BufferedReader(fr1);
-        String line = reader.readLine();
-        int line_n = 1;
-        while (line != null) {
-            line = line.trim();
-            if(!line.isEmpty())
-                if(line.indexOf("#") == 0)
-                    directives.add(line.substring(1));
-                else
-                    directives.add(line);
-            line = reader.readLine();
-            line_n++;
+        String data = reader1.readLine();
+        while(data != null){
+            if(!data.trim().isEmpty())
+                operator.add(data.trim());
+            data = reader1.readLine();
         }
-        fr1.close();
-*/
 
-        //reserved word and etc input
+        file1 = new File("reserved.txt");
+        fr1 = new FileReader(file1);
+        reader1 = new BufferedReader(fr1);
+        data = reader1.readLine();
+        while(data != null){
+            if(!data.trim().isEmpty())
+                reserved.add(data.trim().toLowerCase(Locale.ROOT));
+            data = reader1.readLine();
+        }
+
+
+
+
+
 
 
     }
 
+
+
+
+
+    private void do_step(char c){
+        if(state == 0){
+            if(white.contains(c))
+
+        }
+        if(state == 1){
+
+        }
+        if(state == 0){
+
+        }
+    }
+
+    public void analyse() throws IOException {
+        int c = fr.read();
+        while (c != -1){
+            text += (char)c;
+            text_draft += Character.toLowerCase(c);
+            c = fr.read();
+        }
+        len = text.length();
+        token = new int[len];
+        for(int i = 0; i < len; i++)
+            token[i] = 0;
+
+        int i = 0;
+        while(i < len){
+            int pos1 = text_draft.indexOf("//", i);
+            if(pos1 != -1){
+                int pos2 = text_draft.indexOf("\n");
+                if(pos2 == -1)
+                    pos2 = len;
+                for(int j = pos1; j < pos2; j++)
+                    token[j] = 1;
+                text_draft = to_white(text_draft, pos1, pos2);
+                i = pos2;
+            } else
+                i = len;
+        }
+
+        i = 0;
+        while(i < len){
+            int pos1 = text_draft.indexOf("/*", i);
+            if(pos1 != -1){
+                int pos2 = text_draft.indexOf("*/");
+                if(pos2 == -1)
+                    pos2 = len;
+                for(int j = pos1; j < pos2; j++)
+                    token[j] = 1;
+                text_draft = to_white(text_draft, pos1, pos2);
+                i = pos2;
+            } else
+                i = len;
+        }
+
+        i = 0;
+        while(i < len){
+            int pos1 = text_draft.indexOf("#", i);
+            if(pos1 != -1){
+                int pos_before = text_draft.substring(0, pos1).lastIndexOf("\n");
+                if(pos_before == -1 || is_blank_string(text_draft.substring(pos_before, pos1))){
+                    int pos2 = text_draft.indexOf("\n");
+                    for(int j = pos1; j < pos2; j++)
+                        token[j] = 2;
+                    text_draft = to_white(text_draft, pos1, pos2);
+                    i = pos2;
+                } else
+                    i = pos1 + 1;
+            } else
+                i = len;
+        }
+
+        i = 0;
+        while(i < len){
+            int tmp1 = text_draft.indexOf("'", i);
+            int tmp2 = text_draft.indexOf("\"", i);
+            int pos1;
+            if(tmp1 != -1 && tmp2 != -1)
+                pos1 = Math.min(tmp1, tmp2);
+            if(tmp1 == -1 && tmp2 == -1)
+                i = len;
+            else {
+                if(tmp1 != -1)
+                    pos1 = tmp1;
+                else
+                    pos1 = tmp2;
+                char ch = text_draft.charAt(pos1);
+                int j = pos1;
+                boolean not_ok = true;
+                int pos2 = -1;
+                while(j < len && not_ok){
+                    pos2 = text_draft.indexOf(ch, j);
+                    if(pos2 != -1){
+                        if(text_draft.charAt(pos2 - 1) != '\\')
+                            not_ok = false;
+                        else
+                            j = pos2;
+                    } else
+                        j = len;
+                }
+                if(j < len && pos2 != -1){
+                    set_state(text_draft, pos1, pos2, 3);
+                }
+            }
+
+        }
+
+
+
+
+    }
+
+    private String set_state(String str, int pos1, int pos2, int st){
+        if(pos1 >= pos2)
+            return str;
+        if(pos2 > str.length())
+            pos2 = str.length();
+        for(int j = pos1; j < pos2; j++)
+            token[j] = 2;
+        str = to_white(str, pos1, pos2);
+        return str;
+    }
+
+    private String to_white(String str, int i, int j){
+        if(i >= j)
+            return str;
+        String tmp = "";
+        for(int k = 0; k < j - i; k++)
+            tmp += " ";
+        return str.substring(0, i) + tmp + str.substring(j);
+    }
+
+
+
+
+/*
     public void analyse() throws IOException {
         String line;
         line_num = 1;
@@ -64,6 +233,11 @@ public class lexer {
     }
 
 
+ */
+
+
+
+
     private boolean is_directive_line(String line) throws IOException {
         int first_hashtag = line.indexOf("#");
         if (first_hashtag != -1){
@@ -79,6 +253,7 @@ public class lexer {
         return string == null || string.trim().isEmpty();
     }
 
+    /*
     private void process_line(String line, int from){
         process_line(line, from, line.length());
     }
@@ -90,17 +265,17 @@ public class lexer {
             cur_line.add(line.substring(from, to));
             cur_color.add(token.clear);
         } else {
-            int pos = line.indexOf("*/");
+            int pos = line.indexOf("");
             if(pos != -1){
                 process_line(line, from, pos);
-                cur_line.add("*/");
+                cur_line.add("");
                 cur_color.add(token.error);
                 process_line(line, pos + 2, to);
             } else {
                 pos = line.indexOf("//");
                 if(pos != -1){
                     process_line(line, from, pos);
-                    cur_line.add("*/");
+                    cur_line.add("");
                     cur_color.add(token.error);
                     process_line(line, pos + 2, to);
                 }
@@ -108,6 +283,7 @@ public class lexer {
         }
     }
 
+*/
 
     //variables
 
@@ -118,21 +294,39 @@ public class lexer {
 
     int state = 0;
 
+    //0 - start of line
+    //1 - white char
+
     //HashSet<String> directives;
     //static String directibes_file = "directives.txt";
 
+    /*
     ArrayList<String> cur_line;
     ArrayList<token> cur_color;
 
 
     ArrayList<ArrayList<String>> out_str;
     ArrayList<ArrayList<token>> color;
-
+    */
+    /*
     enum token {
         clear,
         directive,
         error
-    }
+    }*/
+
+    HashSet<Integer> white;
+    HashSet<Integer> az;
+    HashSet<Integer> az09;
+    HashSet<Integer> az09_;
+    HashSet<String> operator;
+    HashSet<String> reserved;
+
+    String text = "";
+    String text_draft = "";
+    int[] token;
+    int len;
+
 
 
 
